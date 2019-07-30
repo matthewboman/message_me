8f9bc08b0a71f8c17fff706e06489e6bfc6a9c71
crashspingfield <ccrsh@riseup.net>
2019-07-29 22:18:34 -0400

    add semantic-ui; build chatroom page

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,7 @@
+class ChatroomController < ApplicationController
+
+  def show
+
+  end
+
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  # Get
+  def new
+  end
+
+  # Post
+  def create
+
+  end
+
+  # Delete
+  def destroy
+
+  end
+  
+end
